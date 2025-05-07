1ce59bc7e2ba7356fd14846024595ea1cf51951e
Prince Vegeta <kurumasagemode@gmail.com>
2025-05-07 21:52:48 +0530

    added dynamic data for restaurents card

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import react from "react";
 import reactDOM from "react-dom/client";
+import data from "./data/index.json";
 /*
  - Header
     -logo
@@ -37,27 +38,67 @@ const Header = () => {
   );
 };
 
-const ResCard = () => {
+const Rating = ({ rating }) => {
+  return (
+    <span>
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        aria-hidden="true"
+        strokecolor="rgba(2, 6, 12, 0.92)"
+        fillcolor="rgba(2, 6, 12, 0.92)"
+      >
+        <circle
+          cx="10"
+          cy="10"
+          r="9"
+          fill="url(#StoreRating20_svg__paint0_linear_32982_71567)"
+        ></circle>
+        <path
+          d="M10.0816 12.865C10.0312 12.8353 9.96876 12.8353 9.91839 12.865L7.31647 14.3968C6.93482 14.6214 6.47106 14.2757 6.57745 13.8458L7.27568 11.0245C7.29055 10.9644 7.26965 10.9012 7.22195 10.8618L4.95521 8.99028C4.60833 8.70388 4.78653 8.14085 5.23502 8.10619L8.23448 7.87442C8.29403 7.86982 8.34612 7.83261 8.36979 7.77777L9.54092 5.06385C9.71462 4.66132 10.2854 4.66132 10.4591 5.06385L11.6302 7.77777C11.6539 7.83261 11.706 7.86982 11.7655 7.87442L14.765 8.10619C15.2135 8.14085 15.3917 8.70388 15.0448 8.99028L12.7781 10.8618C12.7303 10.9012 12.7095 10.9644 12.7243 11.0245L13.4225 13.8458C13.5289 14.2757 13.0652 14.6214 12.6835 14.3968L10.0816 12.865Z"
+          fill="white"
+        ></path>
+        <defs>
+          <linearGradient
+            id="StoreRating20_svg__paint0_linear_32982_71567"
+            x1="10"
+            y1="1"
+            x2="10"
+            y2="19"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#21973B"></stop>
+            <stop offset="1" stopColor="#128540"></stop>
+          </linearGradient>
+        </defs>
+      </svg>
+    </span>
+  );
+};
+
+const ResCard = ({ restaurantDetails }) => {
+  const { name, rating, cuisine, deliveryTime, image } = restaurantDetails;
   return (
     <div className="res-card">
-      <img
-        className="res-card-image"
-        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/12/9/15e625b7-6a11-4d01-b82c-727eb90e5af6_798812.JPG"
-        alt="res-card-image"
-      />
+      <img className="res-card-image" src={image} alt="res-card-image" />
       <div className="res-card-details">
-        <h2 className="res-card-name">KFC</h2>
+        <h2 className="res-card-name">{name}</h2>
         <div className="res-card-rating-and-time">
-          <h3 className="res-card-rating">4.4</h3>
-          <h3 className="res-card-time">20 -25 mins</h3>
+          <h3 className="res-card-rating">
+            <Rating rating={rating} />
+          </h3>
+          <h3 className="res-card-time">{deliveryTime}</h3>
         </div>
-        <h3 className="res-card-cuisines">Burgers,Fast Food,Rolls & Wraps</h3>
+        <h3 className="res-card-cuisines">{cuisine.join(",")}</h3>
       </div>
     </div>
   );
 };
 
 const Body = () => {
+  const { restaurants } = data;
   return (
     <div className="body">
       <div className="search">
@@ -65,20 +106,12 @@ const Body = () => {
         <button className="search-btn">Search</button>
       </div>
       <div className="res-container">
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
-        <ResCard />
+        {restaurants.map((restaurantDetails) => (
+          <ResCard
+            key={restaurantDetails.id}
+            restaurantDetails={restaurantDetails}
+          />
+        ))}
       </div>
     </div>
   );
